c9e47943960e822c10e8679bc3597ea16d61a398
Amlakie Abebaw <amlakieabebaw96@gmail.com>
2026-04-10 00:15:20 +0300

    Clean initial commirrrrr

--- dist/routes/wallet.js
+++ dist/routes/wallet.js
@@ -1,22 +1,29 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const router = express_1.default.Router();
-// Get wallet info
-//router.get('/', authenticate, getWallet);
-// Get transaction history
-//router.get('/transactions', authenticate, getTransactions);
-// Get winnings
-//router.get('/winnings', authenticate, getWinnings);
-// Deposit
-//router.post('/deposit', paymentLimiter, authenticate, initializeDeposit);
-// Verify deposit (after redirect from Chapa)
-//router.get('/deposit/verify/:tx_ref', authenticate, verifyDeposit);
-// Webhook (Chapa calls this)
-//router.post('/webhook', handleWebhook);
-// Withdraw funds
-//router.post('/withdraw', paymentLimiter, authenticate, requestWithdrawal);
-exports.default = router;
+// import express from 'express';
+// import { 
+//   getWallet,
+//   getTransactions,
+//   initializeDeposit,
+//   verifyDeposit,
+//   handleWebhook,
+//   requestWithdrawal,
+//   getWinnings
+// } from '../controllers/walletController';
+// import { authenticate } from '../middleware/auth';
+// import { paymentLimiter } from '../middleware/rateLimit';
+// const router = express.Router();
+// // Get wallet info
+// //router.get('/', authenticate, getWallet);
+// // Get transaction history
+// //router.get('/transactions', authenticate, getTransactions);
+// // Get winnings
+// //router.get('/winnings', authenticate, getWinnings);
+// // Deposit
+// //router.post('/deposit', paymentLimiter, authenticate, initializeDeposit);
+// // Verify deposit (after redirect from Chapa)
+// //router.get('/deposit/verify/:tx_ref', authenticate, verifyDeposit);
+// // Webhook (Chapa calls this)
+// //router.post('/webhook', handleWebhook);
+// // Withdraw funds
+// //router.post('/withdraw', paymentLimiter, authenticate, requestWithdrawal);
+// export default router;
